284c8557b50f92b20393d6a360be4744e2eadc0c
Evaluator <thrvoid96@gmail.com>
2022-12-14 10:53:08 +0300

    more cleanage

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,13 +6,11 @@ using System;
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager instance;
-    public static GameState gamestate;
     [NonSerialized] public LevelAssetCreate levelAsset;
     
 
     private void Awake()
     {
-        gamestate = GameState.BeforeStart;
         instance = this;
         SetValues();
     }
@@ -23,23 +21,6 @@ public class LevelManager : MonoBehaviour
         levelAsset = Resources.Load<LevelAssetCreate>("Scriptables/LevelAsset");
     }
     
-    public void Victory(float delay = 0.9f)
-    {
-        Debug.Log("VICTORY");
-        gamestate = GameState.Victory;
-    }
 
-    //-------------------------------------------------------------------//
-    public void Fail()
-    {
-        Debug.Log("FAILED");
-        gamestate = GameState.Failed;
-    }
 
-    //----------------------------------------------------------------------------------------//
-    public int FindTotalScore()
-    {
-        int totalScore = GameManager.Level * 10 + 50;
-        return totalScore;
-    }
 }
--- Assets/Scripts/UI Elements/InputPanel.cs
+++ Assets/Scripts/UI Elements/InputPanel.cs
@@ -3,36 +3,23 @@ using UnityEngine.EventSystems;
 public class InputPanel : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler, IBeginDragHandler
 {
     public static InputPanel instance;
-    [System.NonSerialized] public GameObject tutorial;
     [System.NonSerialized] public float horizontal;
     Vector2 _lastPosition = Vector2.zero;
     public static float valX;
     private void Awake()
     {
         instance = this;
-        //tutorial = transform.GetChild(0).gameObject;
     }
     public void OnPointerDown(PointerEventData eventData)
     {
-        if (LevelManager.gamestate == GameState.BeforeStart)
-        {
-            _lastPosition = eventData.position;
-            PlayArea.instance.GameStart();
-        }
-        else if(LevelManager.gamestate == GameState.Normal)
-        {
-            _lastPosition = eventData.position;
-        }
+        _lastPosition = eventData.position;
     }
     public void OnDrag(PointerEventData eventData)
     {
-        if (LevelManager.gamestate == GameState.Normal)
-        {
-            Vector2 direction = eventData.position - _lastPosition;
-            horizontal = direction.x * 2 / Screen.width;
-            valX = Mathf.Lerp(valX, horizontal, 0.4f);
-            _lastPosition = eventData.position;
-        }
+        Vector2 direction = eventData.position - _lastPosition;
+        horizontal = direction.x * 2 / Screen.width;
+        valX = Mathf.Lerp(valX, horizontal, 0.4f);
+        _lastPosition = eventData.position;
     }
     public void OnPointerUp(PointerEventData eventData)
     {
@@ -41,9 +28,6 @@ public class InputPanel : MonoBehaviour, IPointerDownHandler, IPointerUpHandler,
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        if (LevelManager.gamestate == GameState.Normal)
-        {
-            _lastPosition = eventData.position;
-        }
+        _lastPosition = eventData.position;
     }
 }
